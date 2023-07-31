9201cf3f2177863cfae25f27227dc16868224435
Jose Andres Salazar <joseandres61@gmail.com>
2023-07-31 07:24:20 -0400

    chore: set User model in a gorm model

--- models/models.go
+++ models/models.go
@@ -1,11 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	Id         int
-	Name       string
-	LastName   string
-	Profession string
-	Created_at time.Time
+	gorm.Model
+	Id         int       `gorm:"primaryKey"`
+	Name       string    `gorm:"type:varchar(50)"`
+	LastName   string    `gorm:"type:varchar(50)"`
+	Email      string    `gorm:"unique_index"`
+	Profession string    `gorm:"type:varchar(50)"`
+	Created_at time.Time `gorm:"autoCreateTime:true"`
 }
